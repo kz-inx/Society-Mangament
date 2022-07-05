b5ed7abc2acc27cabdf8d2d9ac9fa96ff52c06ee
kz-inx <kunal.inexture@gmail.com>
2022-07-05 11:11:00 +0530

    Working on email func

--- societymangament/settings.py
+++ societymangament/settings.py
@@ -208,7 +208,7 @@ SIMPLE_JWT = {
 django_heroku.settings(locals())
 
 #smtp
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
